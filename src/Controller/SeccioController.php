538f23f023c1a27f9d4272c5bd65b1bdcf6cf7c3
matecijas <matecijas@gmail.com>
2025-01-13 15:50:12 +0000

    ya si se

--- src/Controller/SeccioController.php
+++ src/Controller/SeccioController.php
@@ -41,26 +41,35 @@ class SeccioController extends AbstractController
         ),
     );
 
+    #[Route('/dades_seccio', name: 'dades_seccio_error')]
+    public function fitxa_error()
+    {
+         return $this->render("dades_seccio.html.twig", [
+            'codi' => $this->dades[0]['codi'],
+            'nom' => $this->dades[0]['nom'],
+            'any' => $this->dades[0]['any'],
+            'descripcio' => $this->dades[0]['descripcio'],
+            'articles' => $this->dades[0]['articles']
+        ]);
+    }
 
     #[Route('/dades_seccio/{codi}', name: 'dades_seccio')]
     public function fitxa($codi)
     {
+
         $resultat = array_filter($this->dades, function ($dades) use ($codi) {
             return $dades["codi"] == $codi;
         });
 
         if (count($resultat) > 0) {
-            $resposta = "";
             $resultat = array_shift($resultat); // Torna el primer element
-            $resposta .= "<ul>
-                            <li>" . $resultat["nom"] . "</li>
-                            <li>" . $resultat["codi"] . "</li>
-                            <li>" . $resultat["any"] . "</li>
-                             <li>" . implode(", ", $resultat["articles"]) . "</li>
-                            
-                        </ul>";
-
-            return new Response("<html><body>$resposta</body></html>");
+            return $this->render('dades_seccio.html.twig', array(
+                'codi' => $resultat['codi'],
+                'nom' => $resultat['nom'],
+                'any' => $resultat['any'],
+                'descripcio' => $resultat['descripcio'],
+                'articles' => $resultat['articles']
+            ));
         } else {
             return new Response("Seccio no existeix");
     }
